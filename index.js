7c14b4ecaf11af190a4b18fe9ae71bb98413484b
xecuoniter <beratgazi044@gmail.com>
2026-04-16 20:48:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot çalışıyor");
+});
+
+app.listen(3000, () => {
+  console.log("Web server aktif");
+});
+
 const { Client, GatewayIntentBits, SlashCommandBuilder, REST, Routes } = require('discord.js');
 const fs = require('fs');
 
