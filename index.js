f4c4b856e45070de6abf19a34d18de2589c05a46
xecuoniter <beratgazi044@gmail.com>
2026-04-16 20:51:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ app.get("/", (req, res) => {
   res.send("Bot çalışıyor");
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Web server aktif");
 });
 
